6bd0f747ad59b488415b78316a43fa3f7458acc4
alxmkh <walx404@gmail.com>
2020-02-04 19:32:00 +0300

    test

---
+++ lesson_7/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string' * 9);
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(persone);
+
+let arr = ['plum.png','orange.jpeg','apple.bmp']
+//alert("Hello World!");
+//let answer = confirm("Are you here?");
+//console.log(answer);
+//let answer = +prompt("Age > 18?", "Yes!");
+//console.log(typeof(answer));
+//console.log(answer);
